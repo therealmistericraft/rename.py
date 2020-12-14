92669db4223020db8338d31a094722c724111e6c
therealmistericraft <65766422+therealmistericraft@users.noreply.github.com>
2020-12-14 20:03:37 +0100

    Wrote first code
    Wrote first simple code, which should  work standalone, but is not tested, because I want to install a VM first.

--- rename.py
+++ rename.py
@@ -1,1 +1,14 @@
-#TODO: Write Code
+import os
+from os import listdir
+from os.path improt isfile, join
+
+removal = "IMG_"
+path = "/"
+files = [f for f in listdir(path) if isfile(join(path, f)]
+nFiles = []
+
+i=0
+while i != files.len():
+    nFile = files[i].replace(removal, "")
+    os.rename(files[i], nFile)
+    i++
